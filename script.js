053fcbe10b0fd0b5f6b9fcecd10b44db2520958e
Dennis Amparo <dennisamparo11@gmail.com>
2020-02-05 17:11:16 -0700

    pwg2

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+alert("the length of your password should between 8 to 128 characters");
+
+var pwLength = prompt("how long is your password?");
+
+if (pwLength <= 128 && pwLength >= 8) {
+  alert("password length approved");
+}
+else{
+    alert("password length denied. Please try again.");
+}
+// once I add a loop, need to kill the loop at the point whenever the else option is selected then have it begin from the beginning
+
+var lowCaseCH = confirm("would you like to use lower case characters?");
+
+if (lowCaseCH) {
+  alert("great");
+}
+else {
+  alert("You must use at least one lower case character.");
+}
+
+var uppCaseCH = confirm("would you like to use upper case characters?");
+
+if (uppCaseCH) {
+  alert("great!");
+}
+else {
+  alert("You must use at least one upper case character.");
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
