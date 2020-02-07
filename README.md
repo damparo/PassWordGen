0414ab11095fe0768af77a279e05fd7b67e8c92f
Dennis Amparo <dennisamparo11@gmail.com>
2020-02-06 17:03:40 -0700

    pwg3

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PassWordGen
+The assignment was about using javascript to create a password generator.  
+HTML/CSS was provided so the main focus was setting up JS to allow the page 
+to interact with the user and have it generate a password.
+I created a roadmap on paper outlining the steps that are needed to get the 
+password. I was able to create the prompts and generate if/else statements but I had
+a hard time putting together a working function to assemble a password.
+My next step is to rework my functions to generate a password.
--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
+//this is where i begin
 alert("the length of your password should between 8 to 128 characters");
 
 var pwLength = prompt("how long is your password?");
-
+//set up my condition for the password length
 if (pwLength <= 128 && pwLength >= 8) {
   alert("password length approved");
 }
 else{
     alert("password length denied. Please try again.");
 }
-// once I add a loop, need to kill the loop at the point whenever the else option is selected then have it begin from the beginning
-
+// once I add a loop, I need to kill the loop at the point whenever the else option is selected then have it begin from the beginning
+//set up my prompts
 var lowCaseCH = confirm("would you like to use lower case characters?");
 
 if (lowCaseCH) {
@@ -31,15 +30,20 @@ else {
   alert("You must use at least one upper case character.");
 }
 
+var specialCH = confirm("would you like to use special characters?");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+if(specialCH){
+  alert("great!!");
+}
+else{
+  alert("You must use at least one special character.");
+}
+//still figuring out the functions. Will add them here
 
-  passwordText.value = password;
+var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+for (var i = 0; i < alpha.length; i++) {
+    console.log(alpha[i]);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
